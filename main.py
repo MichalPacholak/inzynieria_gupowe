ca8f0d60d48bd361d0ba0f2bfc762a5b09b93a53
Anna Kosinska <akosinska36@gmail.com>
2023-06-24 10:30:07 +0200

    zadanie B

--- main.py
+++ main.py
@@ -11,9 +11,27 @@ def objetosc_szescianu(a):
 def powierzchnia_szescianu(a):
     return 6*a**2
 
-#test dla objętości kuli
+#test dla objętości stozka
 
-spodziewany_wynik = 4/3 * math.pi * (promien_kuli ** 3)
+spodziewany_wynik = 1/3 * math.pi * h_stozka * (promien_stozka ** 2)
 
-assert oblicz_objetosc_kuli(promien_kuli) == spodziewany_wynik
+assert oblicz_objetosc_stozka(promien_kuli, h_stozka) == spodziewany_wynik
+
+#objetosc ostroslupa
+a = 4
+
+h = 6
+
+objetosc_ostroslupa = (1/3) * a**2 * h
+
+
+
+
+# pole powierzchni ostrosłupa
+
+a = 4
+
+h = 6
+
+pole_powierzchni_ostroslupa = a**2 + (a * math.sqrt((a**2)/4 + h**2)) + (a * math.sqrt((a**2)/4 + h**2))
 
